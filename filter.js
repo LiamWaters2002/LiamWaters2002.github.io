c621e4540af1e4b2814a5d6ec107ca396858e47e
Liam Waters <liamwats@outlook.com>
2023-05-08 17:18:32 +0100

    Move Directory Location

--- filter.js
+++ filter.js
@@ -1,69 +1,69 @@
-const allButton = document.getElementById('btnAll');
-const gamesButton = document.getElementById('btnGames');
-const softwareButton = document.getElementById('btnSoftware');
-const websitesButton = document.getElementById('btnWebsites');
+// const allButton = document.getElementById('btnAll');
+// const gamesButton = document.getElementById('btnGames');
+// const softwareButton = document.getElementById('btnSoftware');
+// const websitesButton = document.getElementById('btnWebsites');
 
-const appsButton = document.getElementById('btnApps');
-const modelsButton = document.getElementById('btnModels');
-const otherButton = document.getElementById('btnOther');
+// const appsButton = document.getElementById('btnApps');
+// const modelsButton = document.getElementById('btnModels');
+// const otherButton = document.getElementById('btnOther');
 
 
-console.log(gamesButton);
+// console.log(gamesButton);
 
-allButton.addEventListener('click', () => {
-  projectElements.forEach(element => {
-    element.style.display = 'block';
-  });
-});
+// allButton.addEventListener('click', () => {
+//   projectElements.forEach(element => {
+//     element.style.display = 'block';
+//   });
+// });
 
-function filterCategories(className) {
-    const categoryContainer = document.getElementById("category-grid");
+// function filterCategories(className) {
+//     const categoryContainer = document.getElementById("category-grid");
 
-    const categories = categoryContainer.getElementsByClassName('category');
+//     const categories = categoryContainer.getElementsByClassName('category');
 
-    for (let i = 0; i < categories.length; i++) {
-        if (categories[i].id == className) {
-            categories[i].style.display = 'block';
-        } 
-        else {
-            categories[i].style.display = 'none';
-        }
-    }
-}
+//     for (let i = 0; i < categories.length; i++) {
+//         if (categories[i].id == className) {
+//             categories[i].style.display = 'block';
+//         } 
+//         else {
+//             categories[i].style.display = 'none';
+//         }
+//     }
+// }
 
-function displayAll() {
-    const categoryContainer = document.getElementById("category-grid");
-    const categories = categoryContainer.getElementsByClassName('category');
+// function displayAll() {
+//     const categoryContainer = document.getElementById("category-grid");
+//     const categories = categoryContainer.getElementsByClassName('category');
     
-    for (let i = 0; i < categories.length; i++) {
-            categories[i].style.display = 'block';
-    }
-}
+//     for (let i = 0; i < categories.length; i++) {
+//             categories[i].style.display = 'block';
+//     }
+// }
 
-allButton.addEventListener('click', function() {
-    displayAll();
-});
+// allButton.addEventListener('click', function() {
+//     displayAll();
+// });
 
-gamesButton.addEventListener('click', function() {
-    filterCategories("games")
-});
+// gamesButton.addEventListener('click', function() {
+//     filterCategories("games")
+// });
   
-softwareButton.addEventListener('click', function() {
-    filterCategories("software")
-});
+// softwareButton.addEventListener('click', function() {
+//     filterCategories("software")
+// });
 
-websitesButton.addEventListener('click', function() {
-    filterCategories("websites")
-});
+// websitesButton.addEventListener('click', function() {
+//     filterCategories("websites")
+// });
 
-appsButton.addEventListener('click', function() {
-    filterCategories("apps")
-});
+// appsButton.addEventListener('click', function() {
+//     filterCategories("apps")
+// });
   
-modelsButton.addEventListener('click', function() {
-    filterCategories("models")
-});
+// modelsButton.addEventListener('click', function() {
+//     filterCategories("models")
+// });
 
-otherButton.addEventListener('click', function() {
-    filterCategories("other")
-});
+// otherButton.addEventListener('click', function() {
+//     filterCategories("other")
+// });
