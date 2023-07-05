a66d4e93c3d8626a74fd0d7a46271193c1fc98e1
Liam Waters <liamwats@outlook.com>
2023-07-05 20:04:06 +0100

    Filtering Buttons Done

--- js/filter.js
+++ js/filter.js
@@ -1,69 +1,15 @@
-// const allButton = document.getElementById('btnAll');
-// const gamesButton = document.getElementById('btnGames');
-// const softwareButton = document.getElementById('btnSoftware');
-// const websitesButton = document.getElementById('btnWebsites');
+const checkboxes = document.querySelectorAll('.filter-checkbox input[type="checkbox"]');
 
-// const appsButton = document.getElementById('btnApps');
-// const modelsButton = document.getElementById('btnModels');
-// const otherButton = document.getElementById('btnOther');
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', () => {
+    const parentCheckbox = checkbox.parentNode;
 
-
-// console.log(gamesButton);
-
-// allButton.addEventListener('click', () => {
-//   projectElements.forEach(element => {
-//     element.style.display = 'block';
-//   });
-// });
-
-// function filterCategories(className) {
-//     const categoryContainer = document.getElementById("category-grid");
-
-//     const categories = categoryContainer.getElementsByClassName('category');
-
-//     for (let i = 0; i < categories.length; i++) {
-//         if (categories[i].id == className) {
-//             categories[i].style.display = 'block';
-//         } 
-//         else {
-//             categories[i].style.display = 'none';
-//         }
-//     }
-// }
-
-// function displayAll() {
-//     const categoryContainer = document.getElementById("category-grid");
-//     const categories = categoryContainer.getElementsByClassName('category');
-    
-//     for (let i = 0; i < categories.length; i++) {
-//             categories[i].style.display = 'block';
-//     }
-// }
-
-// allButton.addEventListener('click', function() {
-//     displayAll();
-// });
-
-// gamesButton.addEventListener('click', function() {
-//     filterCategories("games")
-// });
-  
-// softwareButton.addEventListener('click', function() {
-//     filterCategories("software")
-// });
-
-// websitesButton.addEventListener('click', function() {
-//     filterCategories("websites")
-// });
-
-// appsButton.addEventListener('click', function() {
-//     filterCategories("apps")
-// });
-  
-// modelsButton.addEventListener('click', function() {
-//     filterCategories("models")
-// });
-
-// otherButton.addEventListener('click', function() {
-//     filterCategories("other")
-// });
+    if (checkbox.checked) {
+      parentCheckbox.classList.add('enabled');
+      parentCheckbox.classList.remove('disabled');
+    } else {
+      parentCheckbox.classList.remove('enabled');
+      parentCheckbox.classList.add('disabled');
+    }
+  });
+});
