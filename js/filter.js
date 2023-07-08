1ee302c611a9e2573f21ecbb1662bb63d5bba3e7
Liam Waters <liamwats@outlook.com>
2023-07-08 16:10:21 +0100

    Working Filtering System

--- js/filter.js
+++ js/filter.js
@@ -13,3 +13,28 @@ checkboxes.forEach((checkbox) => {
     }
   });
 });
+
+const sliderCards = document.querySelectorAll('.swiper-slide');
+
+
+function updateCardVisibility() {
+    const enabledFilters = Array.from(checkboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.parentNode.querySelector('.checkbox-text').textContent.trim());
+  
+    sliderCards.forEach((card) => {
+      const cardType = card.dataset.cardtype;
+  
+      const isFilterSelected = enabledFilters.includes(cardType);
+  
+      if (isFilterSelected) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  }
+  
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', updateCardVisibility);
+  });
