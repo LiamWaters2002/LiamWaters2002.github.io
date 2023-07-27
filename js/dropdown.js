ea1c3edd8fec27fab157a485174f45d1b40e9308
Liam Waters <liamwats@outlook.com>
2023-07-27 04:25:30 +0100

    Fixed swiper buttons

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,12 @@
 var menuIcon = document.querySelector('.menu-icon');
 var dropdownMenu = document.querySelector('.dropdown-menu');
+var closeMenu = document.querySelector('.menu-close-btn');
+
 
 menuIcon.addEventListener('click', function() {
+  dropdownMenu.classList.toggle('show');
+});
+
+closeMenu.addEventListener('click', function() {
   dropdownMenu.classList.toggle('show');
 });
